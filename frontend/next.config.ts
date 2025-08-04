3c4ea4d4acdaa3019cbad50eba0fbfcd772ae45f
kairat <k_kurmanov@list.ru>
2025-08-04 17:10:10 +0600

    chore: восстановлено кэширование статики

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -23,56 +23,56 @@ const nextConfig: NextConfig = {
     eslint: {
         ignoreDuringBuilds: true,
     },
-    // async headers() {
-    //     return [
-    //         {
-    //             source: '/:path*.(jpg|jpeg|png|gif|webp|svg|ico)',
-    //             headers: [
-    //                 {
-    //                     key: 'Cache-Control',
-    //                     value: 'public, max-age=31536000, immutable'
-    //                 }
-    //             ]
-    //         },
-    //         {
-    //             source: '/:path*.(js|css)',
-    //             headers: [
-    //                 {
-    //                     key: 'Cache-Control',
-    //                     value: 'public, max-age=31536000, immutable'
-    //                 }
-    //             ]
-    //         },
-    //         {
-    //             source: '/:path*.(woff|woff2|ttf|otf|eot)',
-    //             headers: [
-    //                 {
-    //                     key: 'Cache-Control',
-    //                     value: 'public, max-age=31536000, immutable'
-    //                 }
-    //             ]
-    //         },
-    //         {
-    //             source: '/_next/static/:path*',
-    //             headers: [
-    //                 {
-    //                     key: 'Cache-Control',
-    //                     value: 'public, max-age=31536000, immutable'
-    //                 }
-    //             ]
-    //         },
-    //         {
-    //             source: '/_next/data/:path*',
-    //             headers: [
-    //                 {
-    //                     key: 'Cache-Control',
-    //                     value: 'public, max-age=60, stale-while-revalidate=3600'
-    //                 }
-    //             ]
-    //         },
-    //
-    //     ]
-    // },
+    async headers() {
+        return [
+            {
+                source: '/:path*.(jpg|jpeg|png|gif|webp|svg|ico)',
+                headers: [
+                    {
+                        key: 'Cache-Control',
+                        value: 'public, max-age=31536000, immutable'
+                    }
+                ]
+            },
+            {
+                source: '/:path*.(js|css)',
+                headers: [
+                    {
+                        key: 'Cache-Control',
+                        value: 'public, max-age=31536000, immutable'
+                    }
+                ]
+            },
+            {
+                source: '/:path*.(woff|woff2|ttf|otf|eot)',
+                headers: [
+                    {
+                        key: 'Cache-Control',
+                        value: 'public, max-age=31536000, immutable'
+                    }
+                ]
+            },
+            {
+                source: '/_next/static/:path*',
+                headers: [
+                    {
+                        key: 'Cache-Control',
+                        value: 'public, max-age=31536000, immutable'
+                    }
+                ]
+            },
+            {
+                source: '/_next/data/:path*',
+                headers: [
+                    {
+                        key: 'Cache-Control',
+                        value: 'public, max-age=60, stale-while-revalidate=3600'
+                    }
+                ]
+            },
+
+        ]
+    },
 };
 
 export default nextConfig;
